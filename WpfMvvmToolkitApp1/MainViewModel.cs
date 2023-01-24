4ccc0e052079a54ea1f0f3168ba56aa5c96e46f1
Kuni <kuni.ujiie@grapecity.com>
2023-01-24 09:22:50 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfMvvmToolkitApp1/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace WpfMvvmToolkitApp1;
+
+public partial class NewMainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FullName))]
+    private string firstName = "太郎";
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FullName))]
+    private string lastName = "葡萄";
+
+    public string FullName => $"{LastName} {FirstName}";
+}
+
+public partial class OldMainViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private string firstName = "太郎";
+    public string FirstName
+    {
+        get => firstName;
+        set
+        {
+            firstName = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FullName)));
+        }
+    }
+
+    private string lastName = "葡萄";
+    public string LastName
+    {
+        get => lastName;
+        set
+        {
+            lastName = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FullName)));
+        }
+    }
+
+    public string FullName => $"{LastName} {FirstName}";
+}
